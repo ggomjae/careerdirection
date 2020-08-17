9f409f2f697df1bd1a8e5177740daa4d8428883e
ggomjae <ggomjae@naver.com>
2020-08-17 16:40:07 +0900

    Node.js + sequelize 연동 성공, models 중 User Schema 설계

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express();
+const port = 3000;
+const dbConfig = require('./config/database')
+const db = require('./models');
+
+// database + sequelize 
+const connection = dbConfig.init();
+dbConfig.connect(connection);
+db.sequelize.sync();
+
+// 미들웨어 
 
 app.get('/',(req,res)=>{
-    res.send('Hello ! start Express');
+  res.send('Hello ! start Express');
 });
 
-app.listen(3000,() => {
-    console.log('Start Server');
+
+//sync()가 실행되면 index.js 내에서 작성된 Sequelize 작업이 실제 DB와 동기화되면서 실행.
+
+app.listen(port, () => {
+  console.log('Start Server');
 });
 
